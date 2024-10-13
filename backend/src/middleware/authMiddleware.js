48820d64006a13e1d2db97d408d4d1a13b601d51
dovineowuor <owuordove@gmail.com>
2024-10-14 02:08:33 +0300

    Add authentication middleware for verifying JWT tokens

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,36 @@
+// src/middleware/authMiddleware.js
+
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel'); // Ensure the path is correct
+
+const authenticate = async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            // Get token from header
+            token = req.headers.authorization.split(' ')[1];
+
+            // Verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            // Get user from the token and attach to the request object
+            req.user = await User.User.findByPk(decoded.id);
+
+            if (!req.user) {
+                return res.status(401).json({ message: 'User not found' });
+            }
+
+            next();
+        } catch (error) {
+            console.error('Authorization error:', error);
+            res.status(401).json({ message: 'Not authorized, token failed' });
+        }
+    }
+
+    if (!token) {
+        return res.status(401).json({ message: 'Not authorized, no token' });
+    }
+};
+
+module.exports = { authenticate };
